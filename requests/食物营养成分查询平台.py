cd8bc45ef4fd6754c42c4938402c35466ab8ab91
李磊 <this.lilei@gmail.com>
2021-05-06 10:49:59 +0800

    feat: 食物营养成分查询平台爬虫增加含量水平字段

--- requests/食物营养成分查询平台.py
+++ requests/食物营养成分查询平台.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
         , '胡萝卜素(Carotene)', '维生素A(Vitamin)', 'α-TE', '硫胺素(Thiamin)', '核黄素(Riboflavin)', '烟酸(Niacin)', '维生素C(Vitamin C)'
         , '钙(Ca)', '磷(P)', '钾(K)', '钠(Na)', '镁(Mg)', '铁(Fe)', '锌(Zn)', '硒(Se)', '铜(Cu)', '锰(Mn)', '碘(I)'
         , '饱和脂肪酸(SFA)', '单不饱和脂肪酸(MUFA)', '多不饱和脂肪酸(PUFA)', '合计(Total)']
-    detail_type = ['含量', '同类排名', '同类均值']
+    detail_type = ['含量', '同类排名', '同类均值', '含量水平']
 
     # 拼接数据库字段
     tr_type_field = ''
@@ -25,7 +25,7 @@ if __name__ == '__main__':
             tr_type_field += '`{}` TEXT COMMENT "{}",'.format(tr_type_field_text, tr_type_field_text)
 
     # 打开数据库连接
-    db = pymysql.connect('192.168.1.128', 'root', '123456', 'test')
+    db = pymysql.connect(host='192.168.1.128', user='root', password='123456', db='test')
     # 创建一个游标对象
     cursor = db.cursor()
     table = '''
@@ -39,7 +39,9 @@ if __name__ == '__main__':
     )
     '''.format(tr_type_field)
     print('table', table)
-    # cursor.execute(table)
+    cursor.execute(table)
+    # 提交
+    db.commit()
 
     base_url = 'https://fq.chinafcd.org'
     data_url = 'https://fq.chinafcd.org/FoodInfoQueryAction!queryFoodInfoList.do?' \
@@ -61,7 +63,7 @@ if __name__ == '__main__':
             json_data = json.loads(requests.get(data_url.format(data_pid, data_id, 1)).text)
             data = json_data['list']
             data_num = int(json_data['totalPages'])
-            print('data num', data_num)
+            print('页数', data_num)
             # 页数大于1则分页获取数据
             if data_num > 1:
                 for num in range(data_num - 1):
@@ -74,24 +76,33 @@ if __name__ == '__main__':
                 trs = html.etree.HTML(requests.get(base_url + '/foodinfo/{}.html'.format(item[0])).text).xpath('//tr')
                 # 删除表格标题
                 trs.pop(0)
-                字段名称 = ''
-                字段占位符 = ''
-                字段值 = [大分类名称, 分类名称, item[2]]
+                字段名称s = ''
+                食物名称 = item[2]
+                字段值 = [大分类名称, 分类名称, 食物名称]
                 for tr_index, tr in enumerate(trs):
+                    # 第一个tr多出一个td作为合并行
+                    偏移量 = 0 if tr_index > 0 else 1
                     tds = tr.xpath('td')
-                    字段名称 += '`{}_含量`,`{}_同类排名`,`{}_同类均值`,'.format(tr_type[tr_index], tr_type[tr_index],
-                                                                  tr_type[tr_index])
-                    字段占位符 += '%s,%s,%s,'
-                    # 含量
-                    字段值.append(tds[2].xpath('string()'))
-
-                    # 同类排名
-                    字段值.append(tds[3].xpath('string()'))
-
-                    # 同类均值
-                    字段值.append(tds[4].xpath('string()'))
-                print('\t字段值', 字段值)
-                sql = 'insert into 食物营养成分查询平台 (大分类名称,分类名称,食物名称,{}) value (%s,%s,%s,{})'.format(字段名称[:-1], 字段占位符[:-1])
+                    字段名称 = tr_type[tr_index]
+                    字段名称s += '`{}_含量`,`{}_同类排名`,`{}_同类均值`,`{}_含量水平`,'.format(字段名称, 字段名称, 字段名称, 字段名称)
+                    项目 = tds[0 + 偏移量].xpath('string()')
+                    含量 = tds[1 + 偏移量].xpath('string()')
+                    同类排名 = tds[2 + 偏移量].xpath('string()')
+                    同类均值 = tds[3 + 偏移量].xpath('string()')
+                    含量水平_ele = tds[4 + 偏移量].xpath('img/@alt')
+                    含量水平 = 含量水平_ele[0] if 含量水平_ele else None
+                    print(
+                        '食物名称', 食物名称,
+                        '字段名称', 字段名称,
+                        '项目', 项目,
+                        '含量', 含量,
+                        '同类排名', 同类排名,
+                        '同类均值', 同类均值,
+                        '含量水平', 含量水平
+                    )
+                    字段值.extend([含量, 同类排名, 同类均值, 含量水平])
+                字段占位符 = ('%s,%s,%s,%s,' * len(trs))[:-1]
+                sql = 'insert into 食物营养成分查询平台 (大分类名称,分类名称,食物名称,{}) value (%s,%s,%s,{})'.format(字段名称s[:-1], 字段占位符)
                 # print('sql', sql)
                 cursor.execute(sql, tuple(字段值))
                 # 提交
